c446d14231028f91288ea2d37e192308c8b975ba
Jimena <j.miguez@airlst.com>
2024-02-09 09:34:21 +0100

    Extract page main heading into a component

--- pages/index.vue
+++ pages/index.vue
@@ -2,35 +2,7 @@
   <div>
     <section class="bg-primary">
       <div class="container">
-        <ul
-          class="w-fit mx-auto flex flex-col -space-y-12 sm:-space-y-20 md:-space-y-[85px] lg:-space-y-24 xl:-space-y-28"
-        >
-          <li>
-            <h1 class="heading-one font-extrabold heading-animated-one">
-              Grido
-            </h1>
-          </li>
-          <li>
-            <h1
-              class="heading-one font-extrabold heading-animated-two relative"
-            >
-              <span>Gr</span><span class="opacity-0">i</span>
-              <span>do</span>
-              <img
-                src="/images/ice-cream-hero.png"
-                alt=""
-                class="absolute top-[27px] left-[118px] sm:top-[40px] sm:left-[180px] md:top-[48px] md:left-[194px] lg:left-[226px] lg:top-[58px] xl:left-[280px] xl:top-[70px] w-8 h-15 sm:w-12 sm:h-[100px] md:h-[106px] lg:h-[115px] lg:w-12 xl:h-[145px] xl:w-16"
-              />
-            </h1>
-          </li>
-          <li>
-            <h1
-              class="heading-one font-extrabold heading-animated-three tracking-[11px] sm:tracking-[16px] lg:tracking-[20px] 2xl:tracking-[22px]"
-            >
-              baby
-            </h1>
-          </li>
-        </ul>
+        <BaseHeading />
       </div>
     </section>
   </div>
