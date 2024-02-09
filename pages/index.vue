df77ab00fe350b6baa0592aa3637f610f4c9c270
Jimena <j.miguez@airlst.com>
2024-02-09 09:28:41 +0100

    Fixes to main heading

--- pages/index.vue
+++ pages/index.vue
@@ -2,57 +2,30 @@
   <div>
     <section class="bg-primary">
       <div class="container">
-        <!-- <ul
-          class="w-fit mx-auto flex flex-col -space-y-8 sm:-space-y-12 md:-space-y-16 lg:-space-y-24"
-        >
-          <li>
-            <h1 class="heading-one heading-accent-red font-extrabold">Glasé</h1>
-          </li>
-          <li class="">
-            <h1
-              class="heading-one heading-accent-turqoise font-extrabold flex tracking-[3px] sm:tracking-[5px]"
-            >
-              <span>G</span
-              ><span>
-                <img
-                  src="/images/ice-cream-hero.png"
-                  alt=""
-                  class="w-8 h-15 sm:w-12 sm:h-[190px] md:h-[155px] lg:h-[230px] object-contain mt-7 sm:mt-0 lg:mt-10" /></span
-              ><span>asé</span>
-            </h1>
-          </li>
-          <li>
-            <h1
-              class="heading-one heading-accent-violet font-extrabold tracking-[12px] sm:tracking-[17px] md:tracking-[19px] lg:tracking-[25px] nsm:-mt-3"
-            >
-              Baby
-            </h1>
-          </li>
-        </ul> -->
         <ul
-          class="w-fit mx-auto flex flex-col -space-y-8 sm:-space-y-12 lg:-space-y-16"
+          class="w-fit mx-auto flex flex-col -space-y-12 sm:-space-y-20 md:-space-y-[85px] lg:-space-y-24 xl:-space-y-28"
         >
           <li>
             <h1 class="heading-one font-extrabold heading-animated-one">
-              Glasé
+              Grido
             </h1>
           </li>
           <li>
             <h1
               class="heading-one font-extrabold heading-animated-two relative"
             >
-              <span>G</span><span class="opacity-0">l</span>
-              <span>asé</span>
+              <span>Gr</span><span class="opacity-0">i</span>
+              <span>do</span>
               <img
                 src="/images/ice-cream-hero.png"
                 alt=""
-                class="absolute top-[25px] left-[70px] sm:top-[40px] sm:left-[106px] md:left-[100px] lg:left-[135px] lg:top-[58px] w-8 h-15 sm:w-12 sm:h-[100px] lg:h-[110px]"
+                class="absolute top-[27px] left-[118px] sm:top-[40px] sm:left-[180px] md:top-[48px] md:left-[194px] lg:left-[226px] lg:top-[58px] xl:left-[280px] xl:top-[70px] w-8 h-15 sm:w-12 sm:h-[100px] md:h-[106px] lg:h-[115px] lg:w-12 xl:h-[145px] xl:w-16"
               />
             </h1>
           </li>
           <li>
             <h1
-              class="heading-one font-extrabold heading-animated-three tracking-[11px] sm:tracking-[16px] lg:tracking-[20px]"
+              class="heading-one font-extrabold heading-animated-three tracking-[11px] sm:tracking-[16px] lg:tracking-[20px] 2xl:tracking-[22px]"
             >
               baby
             </h1>
