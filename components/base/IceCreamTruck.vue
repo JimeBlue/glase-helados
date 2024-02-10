d3420e5d28b043b89f0efed8502080f7698b5aa1
Jimena <j.miguez@airlst.com>
2024-02-10 11:46:42 +0100

    Extract truck visual into a component

---
+++ components/base/IceCreamTruck.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="w-fit">
+    <div
+      class="border-2 border-amber-700 w-24 h-24 xl:w-32 xl:h-32 rounded-full flex items-center justify-center"
+    >
+      <IconsTruck id="truck" class="text-amber-700 h-16 w-16 xl:w-20 xl:h-20" />
+    </div>
+    <img
+      src="/images/pink-down-arrow.png"
+      alt=""
+      class="relative -top-16 right-[56px]"
+    />
+  </div>
+</template>
+<script setup>
+window.onscroll = function () {
+  scrollRotate()
+}
+
+const scrollRotate = () => {
+  let image = document.getElementById('truck')
+
+  let rotation = Math.min(window.scrollY / 2, 45)
+
+  image.style.transform = 'rotate(' + -rotation + 'deg)'
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,22 +4,7 @@
       <div class="container">
         <BaseHeading />
         <div class="relative -top-6 xl:-top-16 left-[55%]">
-          <!-- TODO: make the ice truck a component and place it in the page -->
-          <div class="w-fit">
-            <div
-              class="border-2 border-amber-700 w-24 h-24 xl:w-32 xl:h-32 rounded-full flex items-center justify-center"
-            >
-              <IconsTruck
-                id="truck"
-                class="text-amber-700 h-16 w-16 xl:w-20 xl:h-20"
-              />
-            </div>
-            <img
-              src="/images/pink-down-arrow.png"
-              alt=""
-              class="relative -top-16 right-[56px]"
-            />
-          </div>
+          <BaseIceCreamTruck />
         </div>
         <p class="lead">
           la cremosa, refrescante, deliciosa experiencia grido ha llegado<span
@@ -37,16 +22,4 @@
     </section>
   </div>
 </template>
-<script setup>
-window.onscroll = function () {
-  scrollRotate()
-}
-
-const scrollRotate = () => {
-  let image = document.getElementById('truck')
-
-  let rotation = Math.min(window.scrollY / 2, 45)
-
-  image.style.transform = 'rotate(' + -rotation + 'deg)'
-}
-</script>
+<script setup></script>
