bc3281cd0055c7c2d0290feb862b8a0e9988ced6
Jimena <j.miguez@airlst.com>
2024-02-10 11:42:46 +0100

    Animate truck visual

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,10 @@
             <div
               class="border-2 border-amber-700 w-24 h-24 xl:w-32 xl:h-32 rounded-full flex items-center justify-center"
             >
-              <IconsTruck class="text-amber-700 h-16 w-16 xl:w-20 xl:h-20" />
+              <IconsTruck
+                id="truck"
+                class="text-amber-700 h-16 w-16 xl:w-20 xl:h-20"
+              />
             </div>
             <img
               src="/images/pink-down-arrow.png"
@@ -34,4 +37,16 @@
     </section>
   </div>
 </template>
-<script setup></script>
+<script setup>
+window.onscroll = function () {
+  scrollRotate()
+}
+
+const scrollRotate = () => {
+  let image = document.getElementById('truck')
+
+  let rotation = Math.min(window.scrollY / 2, 45)
+
+  image.style.transform = 'rotate(' + -rotation + 'deg)'
+}
+</script>
