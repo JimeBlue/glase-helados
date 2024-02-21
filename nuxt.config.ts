742c374f7847f1e70da9d489cf37491e644d8e8e
Jimena <j.miguez@airlst.com>
2024-02-21 17:20:35 +0100

    Update page title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'Glase Helados',
+      title: 'Grido Helados',
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       meta: [
